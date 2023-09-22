d9600c261694559114443d1852cf522b0db30510
Bekian <Bekian.Cat@gmail.com>
2023-09-21 22:02:46 -0500

    fixed bufio and other stuff

--- app/server.go
+++ app/server.go
@@ -5,40 +5,52 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	// start log
 	fmt.Println("Logs from your program will appear here!")
-
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print(">")
-
-	// Read a line of text (until Enter is pressed)
-	userInput, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
-	userInput = strings.TrimSpace(userInput)
-	if userInput != "PING" {
-		fmt.Println("Wrong input")
-		os.Exit(1)
-	} else {
-		fmt.Println("PONG")
-	}
-
+	// listening on port :6379 using tcp
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	fmt.Println(l)
+	//fmt.Println(l)
+	// log port bind failure
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	// create connection from the listener
+	connection, err := l.Accept()
+	// log connection failure
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	// create a reader on the connection to accept input
+	reader := bufio.NewReader(connection)
+	// read input until newline character (or enter key)
+	message, err := reader.ReadString('\n')
+	// log read failure
+	if err != nil {
+		fmt.Println("Error reading from connection:", err.Error())
+		return
+	}
+	// log input recieved and send response
+	fmt.Println("Received:", message)
+	connection.Write([]byte("+PONG\r\n"))
+
 }
+
+/* function to handle connecitons dynamically
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading from connection:", err.Error())
+		return
+	}
+	fmt.Println("Received:", message)
+	conn.Write([]byte("+PONG\r\n"))
+
+}
+*/
