f63ad4f2cf5714ea23a50762e492b5ebda8182ce
Bekian <Bekian.Cat@gmail.com>
2023-09-21 21:47:40 -0500

    test1

--- app/server.go
+++ app/server.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter your name: ")
+
+	// Read a line of text (until Enter is pressed)
+	userInput, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+	userInput = strings.TrimSpace(userInput)
+	if userInput != "PING" {
+		fmt.Println("Wrong input")
+		os.Exit(1)
+	} else {
+		fmt.Println("PONG")
+	}
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	fmt.Println(l)
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
